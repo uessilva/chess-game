db78173a34bd98e7c30dfb74afb3f1ada2956f08
Uesley Silva <uesleysouza97@gmail.com>
2026-08-05 23:05:59 -0300

    feat: add core types, 0x88 board, and make/unmake move

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,7 @@
-export type Color = 'white' | 'black';
-
-export function opposite(color: Color): Color {
-  return color === 'white' ? 'black' : 'white';
-}
-
 export const START_FEN =
   'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
+
+export * from './board';
+export * from './move';
+export * from './state';
+export * from './types';
