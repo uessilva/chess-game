a7feacf435f7241e0a5b79837f7eedab73051bf5
uessilva <uesleysouza97@gmail.com>
2026-08-06 22:59:58 -0300

    feat: add FEN parsing and serialization

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,7 +1,5 @@
-export const START_FEN =
-  'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-
 export * from './board';
+export * from './fen';
 export * from './move';
 export * from './state';
 export * from './types';
